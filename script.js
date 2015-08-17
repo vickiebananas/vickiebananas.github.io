46f3f6257785aa4f75fa47990ead1cf0ea760b0f
Victoria Chavez <Victoria@Victorias-MacBook-Pro.local>
2015-08-17 14:51:52 -0500

    About added

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-$(document).ready(function(){
-	$("#V").mouseenter(function() {
-		$("#V").attr("src", "self.png");
-	});
-	
-	$("#V").mouseleave(function() {
-		$("#V").attr("src", "V.png");
-	});
-	
-	$(".nav").click(function() {
-		var string = $(this).attr("id");
-		string = string + ".html";
-        $("div").animate({right:'100%'}, {complete: function(){window.location.href=string;}});
-    });
+$("#V").mouseenter(function() {
+	$("#V").attr("src", "self.png");
+});
+
+$("#V").mouseleave(function() {
+	$("#V").attr("src", "V.png");
+});
+
+$(".nav").click(function() {
+	var string = $(this).attr("id");
+	string = string + ".html";
+	$("div").animate({right:'100%'}, {complete: function(){window.location.href=string;}});
 });
